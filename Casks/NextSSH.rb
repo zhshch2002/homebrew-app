fa608123483c3b0c7b95d0e9630508fc35eaaa70
zhshch2002 <zhshch@athorx.com>
2022-10-13 18:54:15 +0800

    Update Codemutex.NextSSH to 2.2.0, 2022-10-13T10:54:14.993Z

--- Casks/NextSSH.rb
+++ Casks/NextSSH.rb
@@ -1,4 +1,4 @@
-cask "nextssh" do
+cask "NextSSH" do
   name "NextSSH"
   desc "简洁直观的SSH客户端"
   homepage "https://codemutex.com/"
@@ -8,9 +8,7 @@ cask "nextssh" do
   sha256 arm:   "cfc328e5630c29910b023fd7ea5c107fb094d567b60cccd049a939896ef80d09",
          intel: "e4c86fde6c178c17249070cf06c906e844600659d36f40ad396bd496217cbd1a"
 
-  url arm:   "https://github.com/codemutex/NextSSH-userland/releases/download/v2.2.0/NextSSH-2.2.0-arm64.dmg",
-      intel: "https://github.com/codemutex/NextSSH-userland/releases/download/v2.2.0/NextSSH-2.2.0.dmg"
-  # url "https://github.com/codemutex/NextSSH-userland/releases/download/v#{version}/NextSSH-#{version}#{arch}.dmg"
+  url "https://github.com/codemutex/NextSSH-userland/releases/download/v#{version}/NextSSH-#{version}#{arch}.dmg"
 
   app "NextSSH.app"
 end
