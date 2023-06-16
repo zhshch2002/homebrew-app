64500f8549bee08c04475a24cf10cad01b510bdf
zhshch2002 <zhshch@athorx.com>
2023-06-16 23:06:49 +0800

    Update NextSSH to vbrew2.5.0-rc.1

--- Casks/nextssh.rb
+++ Casks/nextssh.rb
@@ -1,14 +1,18 @@
 cask "nextssh" do
-  name "nextssh"
-  desc "简洁直观的SSH客户端"
-  homepage "https://codemutex.com/"
+    version "2.5.0-rc.1"
 
-  version "2.2.0"
-  arch arm: "-arm64", intel: ""
-  sha256 arm:   "cfc328e5630c29910b023fd7ea5c107fb094d567b60cccd049a939896ef80d09",
-         intel: "e4c86fde6c178c17249070cf06c906e844600659d36f40ad396bd496217cbd1a"
+    on_arm do
+        url "https://github.com/codemutex/NextSSH-userland/releases/download/v2.5.0-rc.1/NextSSH-v2.5.0-rc.1-mac-arm64.dmg"
+        sha256 "038c168419571ff14675126daf3f99d069dffd89f8358bb9edc6bae2ad68bdf4"
+    end
 
-  url "https://github.com/codemutex/NextSSH-userland/releases/download/v#{version}/NextSSH-#{version}#{arch}.dmg"
+    on_intel do
+        url "https://github.com/codemutex/NextSSH-userland/releases/download/v2.5.0-rc.1/NextSSH-v2.5.0-rc.1-mac-x64.dmg"
+        sha256 "903687412c62cd9f8a61d06a37039d7e3837b3cd2af0013e376041b2cdaa43df"
+    end
 
-  app "NextSSH.app"
+    name "NextSSH"
+    desc "简洁直观的 SSH 客户端"
+
+    app "NextSSH.app"
 end
